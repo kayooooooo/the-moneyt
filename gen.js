214e27d33b1d46475e8d6a4fa60a9c0139ccba8e
kayooooooo <108146877+kayooooooo@users.noreply.github.com>
2025-01-30 21:40:43 -0300

    Update gen.js

--- gen.js
+++ gen.js
@@ -1,1 +1,14 @@
 console.log("testetetetetete");
+
+(function() {
+    var container = document.getElementById("adsContainer2");
+    if (container) { 
+        var script = document.createElement("script");
+        script.src = "//tags.orquideassp.com/tag/79260";
+        container.appendChild(script); // Adiciona dentro da div
+    } else {
+        console.error("Div #adsContainer2 não encontrada!");
+    }
+})();
+
+
