7dc6e18a4dcbdefddb17605908d17fa183ce8b9d
kayooooooo <108146877+kayooooooo@users.noreply.github.com>
2025-01-30 21:42:07 -0300

    Update requestform.js

--- requestform.js
+++ requestform.js
@@ -1,1 +1,12 @@
-console.log("Está funcionando o arquivo externo");
+console.log("testetetetetete");
+
+(function() {
+    var container = document.getElementById("adsContainer2");
+    if (container) { 
+        var script = document.createElement("script");
+        script.src = "//tags.orquideassp.com/tag/79260";
+        container.appendChild(script); // Adiciona dentro da div
+    } else {
+        console.error("Div #adsContainer2 não encontrada!");
+    }
+})();
